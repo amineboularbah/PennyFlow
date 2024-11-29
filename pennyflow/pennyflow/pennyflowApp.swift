b39a59238fc0a5683975e2a4f29b15416a7f8fca
Amine Boularbah <boularbah.amine2000@gmail.com>
2024-11-29 11:52:00 +0100

    Project Setup

---
+++ pennyflow/pennyflow/pennyflowApp.swift
@@ -0,0 +1,20 @@
+//
+//  pennyflowApp.swift
+//  pennyflow
+//
+//  Created by Amine on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct pennyflowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
