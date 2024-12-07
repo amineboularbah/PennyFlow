cd94cd9782d347a3368de100e89179148a9528f6
Amine Boularbah <boularbah.amine2000@gmail.com>
2024-12-07 12:05:51 +0100

    Move bnb UI component to main screen and add navigation logic

---
+++ pennyflow/pennyflow/Views/Calendar/CalendarView.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarView.swift
+//  pennyflow
+//
+//  Created by Amine on 7/12/2024.
+//
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        VStack {
+            Text("CalendarView")
+                .font(.largeTitle)
+        }
+    }
+}
---
+++ pennyflow/pennyflow/Views/Credit Cards/CardsView.swift
@@ -0,0 +1,16 @@
+//
+//  CardsView.swift
+//  pennyflow
+//
+//  Created by Amine on 7/12/2024.
+//
+import SwiftUI
+
+struct CardsView: View {
+    var body: some View {
+        VStack {
+            Text("CardsView")
+                .font(.largeTitle)
+        }
+    }
+}
