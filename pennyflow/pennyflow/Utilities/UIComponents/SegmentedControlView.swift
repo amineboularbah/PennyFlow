41cc0a4fd58a3257d3f368f037278a0f7d8db683
Amine Boularbah <boularbah.amine2000@gmail.com>
2024-12-08 14:18:53 +0100

    Create segement controller view for subscriptions and upcoming bills

--- pennyflow/pennyflow/Utilities/UIComponents/SegmentedControlView.swift
+++ pennyflow/pennyflow/Utilities/UIComponents/SegmentedControlView.swift
@@ -1,56 +1,52 @@
+//
+//  SegmentedControlView.swift
+//  pennyflow
+//
+//  Created by Amine on 8/12/2024.
+//
+
 import SwiftUI
 
 struct SegmentedControlView: View {
-    @State private var selectedTab: Int = 0 // 0 for "Your subscriptions", 1 for "Upcoming bills"
-
+    @Binding var selectedTab: Int  // 0 for "Your subscriptions", 1 for "Upcoming bills"
+    private let cornerRadius: Double = 16
     var body: some View {
         HStack(spacing: 8) {
             // Your Subscriptions Button
-            Button(action: {
-                withAnimation(.easeInOut(duration: 0.3)) {
-                    selectedTab = 0
-                }
-            }) {
-                Text("Your subscriptions")
-                    .font(.headline)
-                    .foregroundColor(selectedTab == 0 ? .white : .gray)
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 12)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(selectedTab == 0 ? Color.gray.opacity(0.4) : Color.clear)
-                    )
-            }
-
             // Upcoming Bills Button
-            Button(action: {
-                withAnimation(.easeInOut(duration: 0.3)) {
-                    selectedTab = 1
-                }
-            }) {
-                Text("Upcoming bills")
-                    .font(.headline)
-                    .foregroundColor(selectedTab == 1 ? .white : .gray)
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 12)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(selectedTab == 1 ? Color.gray.opacity(0.4) : Color.clear)
-                    )
-            }
+            SecondaryButton(
+                title: "Your Subscriptions",
+                action: {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        selectedTab = 0
+                    }
+
+                }, cornerRadius: cornerRadius, isFilled: selectedTab == 0,
+                textColor: selectedTab == 0 ? .white : .gray30)
+            // Upcoming Bills Button
+            SecondaryButton(
+                title: "Upcoming bills",
+                action: {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        selectedTab = 1
+                    }
+
+                }, cornerRadius: cornerRadius, isFilled: selectedTab == 1,
+                textColor: selectedTab == 1 ? .white : .gray30)
         }
-        .padding(8)
+        .padding(.horizontal, 10)
+        .padding(.vertical, 7)
         .background(
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.gray.opacity(0.2)) // Background for the entire segment control
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .fill(Color.grayC)  // Background for the entire segment control
         )
-        .padding(.horizontal, 16) // Optional padding for spacing
+        .padding(.horizontal, 16)  // Optional padding for spacing
     }
 }
 
 struct SegmentedControlView_Previews: PreviewProvider {
     static var previews: some View {
-        SegmentedControlView()
-            .background(Color.black.edgesIgnoringSafeArea(.all)) // Match dark mode background
+        SegmentedControlView(selectedTab: .constant(0))
+            .background(Color.black.edgesIgnoringSafeArea(.all))  // Match dark mode background
     }
 }
