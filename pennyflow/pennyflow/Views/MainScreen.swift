b1d1e5305d6660afc89736eb12b03df4ee462f51
Amine Boularbah <boularbah.amine2000@gmail.com>
2024-12-14 17:27:25 +0100

    Fix main screen error

--- pennyflow/pennyflow/Views/MainScreen.swift
+++ pennyflow/pennyflow/Views/MainScreen.swift
@@ -7,6 +7,7 @@
 import SwiftUI
 
 struct MainScreen: View {
+    @Environment(\.managedObjectContext) var context
     @State private var selectedTab: Int = 0
     @State private var isAddingSubscription = false  // Push to Add Subscription page
 
@@ -17,7 +18,7 @@ struct MainScreen: View {
                 TabView(selection: $selectedTab) {
                     HomeView().tag(0)
                     BudgetsView().tag(1)
-                    CalendarView().tag(2)
+                    CalendarView(context: context).tag(2)
                     CardsView().tag(3)
                 }.padding(.bottom, .bottomInsets)
                 GradientLayer()
